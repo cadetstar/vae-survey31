2ecab417fb8afdc30db255aed799c056d72186f0
Michael Madison <michaelm@simutronics.com>
2011-11-21 23:47:35 -0600

    Added an actual from address.

--- app/mailers/survey_mailer.rb
+++ app/mailers/survey_mailer.rb
@@ -84,6 +84,7 @@ class SurveyMailer < ActionMailer::Base
   def error_message(exception, trace, session, params, env, account, is_live = false, sent_on = Time.now)
     load_settings('vaecorp')
     @recipients    = 'cadetstar@hotmail.com'
+    @from          = 'Survey System <survey@vaecorp.com>'
     @subject       = "Error message: #{env['REQUEST_URI']}"
     @sent_on       = sent_on
     @content_type = "text/html"
