d24a877a1a4a4777d5c5a68c42d394476d089771
Michael Madison <michaelm@simutronics.com>
2011-11-23 11:01:33 -0600

    Added default AM settings

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,1 +1,9 @@
 VaeSurvey31::Application.config.action_mailer.delivery_method = :smtp
+VaeSurvey31::Application.config.action_mailer.smtp_settings = {
+    :address => "mail.vaecorp.com",
+    :port => 8889,
+    :domain => "vaecorp.com",
+    :authentication => :login,
+    :user_name => "thankyou@vaecorp.com",
+    :password => "V@3cs1IT"
+}
