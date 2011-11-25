35e6ec3bc64c71b8c6a90ac49fe7013013bc66e0
Michael Madison <michaelm@simutronics.com>
2011-11-24 21:40:29 -0600

    Fixed the full address in proper place.

--- app/models/company.rb
+++ app/models/company.rb
@@ -18,6 +18,6 @@ class Company < ActiveRecord::Base
   end
 
   def full_address
-    raw [self.address_line_1, self.address_line_2, "#{self.city}, #{self.state} #{self.zip}"].compact.join("<br />")
+    [self.address_line_1, self.address_line_2, "#{self.city}, #{self.state} #{self.zip}"].compact.join("<br />")
   end
 end
